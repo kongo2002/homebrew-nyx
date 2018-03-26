ac7b36a72ca59ff54f88462dfca66c42479fece9
kongo2002 <kongo2002@googlemail.com>
2018-03-26 21:23:10 +0200

    update to 1.9.1

--- Formula/nyx.rb
+++ Formula/nyx.rb
@@ -1,8 +1,8 @@
 class Nyx < Formula
   desc "Process monitoring and supervision tool"
   homepage "https://github.com/kongo2002/nyx/"
-  url "https://github.com/kongo2002/nyx/archive/v1.9.0.tar.gz"
-  sha256 "e08c4164209744d2c8e42dc50af0540a2fdce97d8574f521889ec495e2328edf"
+  url "https://github.com/kongo2002/nyx/archive/v1.9.1.tar.gz"
+  sha256 "44bf6d02e7e4d920ddb2b7748cec3fd12d7dfe0d34766a593c546aa8cdcf91fd"
 
   depends_on "libyaml"
 
